3016a4b892cea8278b2d875c69756159a65a21e0
Pradnya1p <114651151+Pradnya1p@users.noreply.github.com>
2022-09-29 15:18:15 +0530

    Update loginpagesample.java

--- MavanProject1/src/main/java/fbpages/loginpagesample.java
+++ MavanProject1/src/main/java/fbpages/loginpagesample.java
@@ -6,16 +6,16 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class loginpagesample {
-	@FindBy(xpath="//a[text()='Create New Account']")
+	//@FindBy(xpath="//a[text()='Create New Account']")
 	private WebElement createnewacc;
 	public loginpagesample(WebDriver driver)
 	{
 		PageFactory.initElements(driver, this);
 	}
-// 	public void clickcreateAccButton()
-// 	{
-// 		createnewacc.click();
+	public void clickcreateAccButton()
+	{
+		createnewacc.click();
 		
-// 	}
+	}
 
 }
